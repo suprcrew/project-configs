528291b48b6be96c4658b569ac8d07f4f4d13bb4
mbunge <bunge@jungehaie.com>
2021-07-28 23:56:59 +0200

    feat create configurations for typescript, jest.conf.js, tsconfig.base.json

---
+++ configs/eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'max-len': 'off',
+    'consistent-return': 0,
+    complexity: ['error', 5],
+    'no-empty-function': ['error', { allow: ['constructors'] }],
+    '@typescript-eslint/prefer-readonly': 'error',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-underscore-dangle': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'lines-between-class-members': 'off',
+    'class-methods-use-this': 'off',
+    'import/newline-after-import': 'off',
+    'indent': 'off',
+    'import/order': 'off',
+    'no-else-return': 'off',
+    'object-curly-spacing': 'off',
+    'arrow-body-style': 'off',
+    'comma-dangle': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/unbound': 'off',
+    'implicit-arrow-linebreak': 'off'
+  }
+};
---
+++ configs/jest.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "setupFilesAfterEnv": [
+    "jest-extended"
+  ],
+  "testRegex": "((\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "testPathIgnorePatterns": [
+    "systemTests"
+  ],
+  "rootDir": "../../../../../",
+  "testResultsProcessor": "jest-sonar-reporter",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "coverageThreshold": {
+    "global": {
+      "branches": 90,
+      "functions": 90,
+      "lines": 90,
+      "statements": -10
+    }
+  },
+  "globals": {
+    "ts-jest": {
+      "tsconfig": {
+        "experimentalDecorators": true,
+        "emitDecoratorMetadata": true,
+        "module": "commonjs",
+        "declaration": true,
+        "target": "es6",
+        "downlevelIteration": true,
+        "outDir": "./lib"
+      }
+    }
+  }
+};
